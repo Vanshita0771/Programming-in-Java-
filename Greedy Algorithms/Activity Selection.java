ce71dc03d41b81dbc1e4e13c3f16b6b7525491c0
Vanshita Kakkar <78303804+Vanshita0771@users.noreply.github.com>
2022-12-22 22:08:27 +0530

    Update Activity Selection.java

--- Greedy Algorithms/Activity Selection.java
+++ Greedy Algorithms/Activity Selection.java
@@ -1,74 +1,48 @@
-//{ Driver Code Starts
-import java.io.*; 
-import java.util.*; 
-import java.lang.*;
-
-class Main
-{
-    public static void main (String[] args) throws IOException  
-    {
-        
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		//testcases
-		int t = Integer.parseInt(br.readLine().trim());
-		
-		while(t-- > 0)
-		{
-		    String inputLine[] = br.readLine().trim().split(" ");
-		    //size of array
-		    int n = Integer.parseInt(inputLine[0]);
-		    int start[] = new int[n];
-		    int end[] = new int[n];
-		    
-		    //adding elements to arrays start and end
-		    inputLine = br.readLine().trim().split(" ");
-		    for(int i = 0; i < n; i++)
-		     start[i] = Integer.parseInt(inputLine[i]);
-		    
-		    inputLine = br.readLine().trim().split(" ");
-		    for(int i= 0; i < n; i++)
-		      end[i] = Integer.parseInt(inputLine[i]);
-		    
-		    //function call
-		    System.out.println(new Solution().activitySelection(start, end, n));
-		}
-    }
-}
-
-
-// } Driver Code Ends
-
+import java.util.*;
 class Activity implements Comparable<Activity>{
-    int start;
-    int end;
-    Activity(int start,int end){
-        this.start=start;
-        this.end=end;
+    int start,end,index;
+    Activity(int st,int en,int idx){
+        start=st;
+        end=en;
+        index=idx;
     }
-    @Override
-    public int compareTo(Activity a1){
-        return this.end-a1.end;
+    public int compareTo(Activity a){
+        return this.end-a.end;
     }
 }
-class Solution
+public class Main
 {
-    
-    public static int activitySelection(int start[], int end[], int n)
-    {
-        ArrayList<Activity> arr=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            arr.add(new Activity(start[i],end[i]));
-        }
-        int count=1;
-        Collections.sort(arr);
-        int prev=arr.get(0).end;
-      
-        for(int i=1;i<arr.size();i++){
-            if(prev<arr.get(i).start){
-             count++;
-             prev=arr.get(i).end;
-            }
-        }
-        return count;
-    }
+	public static void main(String[] args) {
+	     Scanner sc=new Scanner(System.in);
+	     System.out.println("Enter the number of activities");
+	     int n=sc.nextInt();
+	     int start[]=new int[n];
+	     int end[]=new int[n];
+	     System.out.println("Enter the start time of activities");
+	     for(int i=0;i<n;i++)
+	         start[i]=sc.nextInt();
+	      System.out.println("Enter the end time of activities");
+	     for(int i=0;i<n;i++)
+	         end[i]=sc.nextInt();
+	     Activity[] arr=new Activity[n];
+	     for(int i=0;i<n;i++){
+	         arr[i]=new Activity(start[i],end[i],i);
+	     }
+	     Arrays.sort(arr);
+	     
+	     ArrayList<Integer> act=new ArrayList<>();
+	     act.add(arr[0].index);
+	     int prev=arr[0].end;
+	     for(int i=1;i<n;i++){
+	         
+	         if(arr[i].start>=prev){
+	             act.add(arr[i].index);
+	             prev=arr[i].end;
+	         }
+	     }
+	     for(int i=0;i<act.size();i++){
+	         System.out.println("Act"+act.get(i));
+	     }
+	      
+	}
 }
