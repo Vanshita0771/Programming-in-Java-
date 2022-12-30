6186fbad6dd98e2444e37c8054953a5b1ee9daaa
Vanshita Kakkar <78303804+Vanshita0771@users.noreply.github.com>
2022-12-30 16:39:54 +0530

    Create Count Number of Nodes in Tree.java

---
+++ Binary Trees/Count Number of Nodes in Tree.java
@@ -0,0 +1,49 @@
+import java.util.*;
+class Node{
+   int data;
+   Node left;
+   Node right;
+   Node(int data){
+       this.data=data;
+   }
+}
+class Tree{
+    public Node buildtree(int arr[],int i,int n,Node root){
+        if(i<n){
+            root=new Node(arr[i]);
+            root.left=buildtree(arr,2*i+1,n,root.left);
+            root.right=buildtree(arr,2*i+2,n,root.right);
+        }
+        return root;
+    }
+     public int countNodes(Node root) {
+      
+        if(root.left==null&&root.right==null) return 1;
+        int ans=0;
+        if(root.left!=null)
+         ans+=countNodes(root.left);
+        if(root.right!=null)
+         ans+=countNodes(root.right);
+        return ans+1;
+         
+    }
+     
+}
+public class Main
+{
+	public static void main(String[] args) {
+	   Scanner sc=new Scanner(System.in);
+	   System.out.println("Enter the number of elements to build tree");
+	   int n=sc.nextInt();
+	   int[] arr=new int[n];
+	   System.out.println("Enter the elements");
+	   for(int i=0;i<n;i++){
+	       arr[i]=sc.nextInt();
+	   }
+	   Tree t1=new Tree();
+	   Node root=null;
+	   root=t1.buildtree(arr,0,n,root);
+	   System.out.println(t1.countNodes(root));
+	   
+	}
+}
