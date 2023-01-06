5c8c7a8414aa837bc400aefa79e3545dbcc8dd48
Vanshita Kakkar <78303804+Vanshita0771@users.noreply.github.com>
2023-01-06 14:03:11 +0530

    Update Fractional Knapsack.java

--- Greedy Algorithms/Fractional Knapsack.java
+++ Greedy Algorithms/Fractional Knapsack.java
@@ -1,53 +1,60 @@
 import java.util.*;
-class Item implements Comparable<Item>{
-   int value;
-   int weight;
-   double ratio;
-   Item(int value,int weight){
-       this.value=value;
-       this.weight=weight;
-       ratio=(double) value/weight;
-   }
-   public int compareTo(Item i2){
-       return (int)(i2.ratio-this.ratio);
-   }
-   
+
+class Item implements Comparable<Item> {
+	int value;
+	int weight;
+	double ratio;
+
+	Item(int value, int weight) {
+		this.value = value;
+		this.weight = weight;
+		ratio = (double) value / weight;
+	}
+
+	public int compareTo(Item i2) {
+	
+		if(i2.ratio-this.ratio>0)
+		 return 1;
+		 if(i2.ratio-this.ratio==0)
+		   return 0;
+		 return -1;
+	}
+
 }
-public class Main
-{
+
+public class Solution {
 	public static void main(String[] args) {
-	     Scanner sc=new Scanner(System.in);
-	     System.out.println("Enter the number of items");
-	     int n=sc.nextInt();
-	     int weight[]=new int[n];
-	     int price[]=new int[n];
-	     System.out.println("Enter the weight of items");
-	     for(int i=0;i<n;i++)
-	         weight[i]=sc.nextInt();
-	      System.out.println("Enter the price of items");
-	     for(int i=0;i<n;i++)
-	         price[i]=sc.nextInt();
-	     System.out.println("Enter the weight of bag");
-	     int total_weight=sc.nextInt();
-	     Item[] arr=new Item[n];
-	     for(int i=0;i<n;i++){
-	         arr[i]=new Item(price[i],weight[i]);
-	     }
-	     Arrays.sort(arr);
-	     double total_price=0;
-	     int currWeight=total_weight;
-	     for(int i=0;i<n;i++){
-	         if(currWeight-arr[i].weight>=0){
-	             currWeight-=arr[i].weight;
-	             total_price+=arr[i].value;
-	         }
-	         else{
-	             total_price+=arr[i].ratio*currWeight;
-	             currWeight=0;
-	             break;
-	         }
-	     }
-	     System.out.println("Total value:"+total_price);
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int total_weight = sc.nextInt();
+		int weight[] = new int[n];
+		int price[] = new int[n];
+	
+		for (int i = 0; i < n; i++)
+                {
+			price[i] = sc.nextInt();
+			weight[i] = sc.nextInt();
+		 }
+		Item[] arr = new Item[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = new Item(price[i], weight[i]);
+		}
+		Arrays.sort(arr);
+		double total_price = 0;
+		int currWeight = total_weight;
+		for (int i = 0; i < n; i++) {
+			if (currWeight- arr[i].weight >= 0) {
+				currWeight -= arr[i].weight;
+				total_price += arr[i].value;
+			} else {
+				total_price += arr[i].ratio * currWeight;
+				currWeight = 0;
+				break;
+			}
+		}
+		System.out.println( total_price);
 	}
 }
+
 
