233c3ded7b541744a322573239124b29d7d2bad2
Vanshita Kakkar <78303804+Vanshita0771@users.noreply.github.com>
2023-01-08 18:31:51 +0530

    Create Vertical Sum.java

---
+++ Binary Trees/Vertical Sum.java
@@ -0,0 +1,133 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Solution g = new Solution();
+			
+			    ArrayList <Integer> res = g.verticalSum(root);
+			    for (Integer num : res) System.out.print (num + " ");
+			    System.out.println();
+                t--;
+            
+        }
+    }
+  
+}
+
+
+// } Driver Code Ends
+
+class Pair{
+    int hd;
+    Node node;
+    Pair(int hd,Node node){
+        this.hd=hd;
+        this.node=node;
+    }
+}
+class Solution{
+    public ArrayList <Integer> verticalSum(Node root) {
+        TreeMap<Integer,Integer> map=new TreeMap<>();
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(0,root));
+        
+        while(!q.isEmpty()){
+            Pair temp=q.remove();
+            if(map.containsKey(temp.hd))
+                map.put(temp.hd,map.get(temp.hd)+temp.node.data);
+            else
+              map.put(temp.hd,temp.node.data);
+             if(temp.node.left!=null)
+              q.add(new Pair(temp.hd-1,temp.node.left));
+            if(temp.node.right!=null)
+              q.add(new Pair(temp.hd+1,temp.node.right));
+        }
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int key:map.keySet()){
+            arr.add(map.get(key));
+        }
+        return arr;
+    }
+}
