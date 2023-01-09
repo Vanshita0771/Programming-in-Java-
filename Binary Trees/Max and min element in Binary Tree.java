79d2f45fe23e75ec57fa9622448c9033eb27ff3c
Vanshita Kakkar <78303804+Vanshita0771@users.noreply.github.com>
2023-01-09 11:12:04 +0530

    Create Max and min element in Binary Tree.java

---
+++ Binary Trees/Max and min element in Binary Tree.java
@@ -0,0 +1,148 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*; 
+
+
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+
+class GFG {
+    
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    public static void inorder(Node root){
+        if(root == null)
+            return;
+        
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+		while(t-- > 0){
+		    String s = br.readLine();
+	    	Node root = buildTree(s);
+		    Solution obj = new Solution();
+		    int mx = obj.findMax(root);
+		    int mn = obj.findMin(root);
+		    System.out.println(mx+" "+mn);
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+class Solution{
+     int min=Integer.MAX_VALUE;
+     int max=Integer.MIN_VALUE;
+    public int findMax(Node root){
+         Queue<Node> q=new LinkedList<>();
+         q.add(root);
+         while(!q.isEmpty()){
+             Node temp=q.remove();
+             if(max<temp.data)
+              max=temp.data;
+              if(temp.left!=null)
+               q.add(temp.left);
+              if(temp.right!=null)
+               q.add(temp.right);
+               
+         }
+         return max;
+    }
+    public  int findMin(Node root){
+      
+         Queue<Node> q=new LinkedList<>();
+         q.add(root);
+         while(!q.isEmpty()){
+             Node temp=q.remove();
+             if(min>temp.data)
+              min=temp.data;
+              if(temp.left!=null)
+               q.add(temp.left);
+              if(temp.right!=null)
+               q.add(temp.right);
+               
+         }
+         return min;
+    }
+}
