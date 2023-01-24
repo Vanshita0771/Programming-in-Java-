8f2b3355fc0eec631e8744864978dd6f3fb5ff78
Vanshita Kakkar <78303804+Vanshita0771@users.noreply.github.com>
2023-01-24 18:15:04 +0530

    Create Majority Element.cpp

---
+++ Hashing/Majority Element.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class Geeks
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n =sc.nextInt();
+            int arr[] = new int[n];
+            
+            for(int i = 0; i < n; i++)
+             arr[i] = sc.nextInt();
+             
+           System.out.println(new Solution().majorityElement(arr, n)); 
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    static int majorityElement(int a[], int size)
+    {
+      HashMap<Integer,Integer> map=new HashMap<>();
+      for(int i=0;i<size;i++){
+          if(map.containsKey(a[i])){
+              map.put(a[i],map.get(a[i])+1);
+          }
+          else
+           map.put(a[i],1);
+      }
+      for(int i=0;i<size;i++){
+          if(map.get(a[i])>size/2)
+           return a[i];
+      }
+      return -1;
+    }
+}
+
