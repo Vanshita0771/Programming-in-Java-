70fb3d0a5bbec2e514871172217d72a2086b3291
Vanshita Kakkar <78303804+Vanshita0771@users.noreply.github.com>
2023-01-24 18:26:09 +0530

    Create Maximum repeating number.java

---
+++ Hashing/Maximum repeating number.java
@@ -0,0 +1,60 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int k = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            int ans = new Solution().maxRepeating(arr, n, k);
+            System.out.println(ans);
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    int maxRepeating(int[] arr, int n, int k) {
+        // code here
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i],map.get(arr[i])+1);
+            }
+            else
+             map.put(arr[i],1);
+        }
+        int max=0;
+        int val=0;
+        for(int i=0;i<k;i++){
+            if(map.containsKey(i)&&max<map.get(i))
+             {
+                 max=map.get(i);
+                 val=i;
+             }
+        }
+        return val;
+    }
+}
