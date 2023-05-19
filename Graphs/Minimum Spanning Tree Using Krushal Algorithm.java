1a39d3ce1ace0e14611b07551a1a67f1de0bc402
Vanshita Kakkar <78303804+Vanshita0771@users.noreply.github.com>
2023-05-19 11:23:36 +0530

    Update Minimum Spanning Tree Using Krushal Algorithm.java

--- Graphs/Minimum Spanning Tree Using Krushal Algorithm.java
+++ Graphs/Minimum Spanning Tree Using Krushal Algorithm.java
@@ -1,55 +1,42 @@
-//Krushal Algorithm
 import java.util.*;
-
-public class Main
-{
-    public static boolean union(int a,int b,int parent[]){
-        a=find_parent(a,parent);
-        b=find_parent(b,parent);
-        if(a!=b){
-            parent[a]=b;
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
-    public static int find_parent(int a,int parent[]){
-        if(parent[a]==a){
-            return a;
-        }
-        return find_parent(parent[a],parent);
-    }
-    public static void make_node(int a,int parent[]){
-        parent[a]=a;
-    }
-    public static int MST(int edges[][],int V){
-       int cost=0;
-        int[] parent=new int[V];
-        Arrays.fill(parent,-1);
-        for(int i=0;i<V;i++){
-            make_node(i,parent);
-        }
-        for(int i=0;i<V-1;i++){
-            if(union(edges[i][0],edges[i][1],parent)){
-                cost+=edges[i][2];
-            }
-        }
-        return cost;
-    }
+public class Main {
+   public static int find(int a,int[] parent){
+	   if(a==parent[a]) return a;
+	   return find(parent[a], parent);
+   }
+   public static boolean union(int a,int b,int[] parent){
+	    a=find(a,parent);
+	    b=find(b,parent);
+	   if(a==b) return false;
+	   parent[a]=b;
+	   return true;
+   }
 	public static void main(String[] args) {
-	   int V,E;
-	   Scanner sc=new Scanner(System.in);
-	   V=sc.nextInt();
-	   E=sc.nextInt();
-	   int edges[][]=new int[E][3];
-	  
-	   for(int i=0;i<edges.length;i++){
-	       edges[i][0]=sc.nextInt();
-	       edges[i][1]=sc.nextInt();
-	       edges[i][2]=sc.nextInt();
-	   }
-	    Arrays.sort(edges,(a,b)->a[2]-b[2]);
-	   System.out.print(MST(edges,V));
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t>0){ 
+			int v=sc.nextInt();
+			int e=sc.nextInt();
+			int[][] edges=new int[e][3];
+			for(int i=0;i<e;i++)
+			{
+				edges[i][0]=sc.nextInt();
+				edges[i][1]=sc.nextInt();
+				edges[i][2]=sc.nextInt();			
+			}
+			Arrays.sort(edges,(a,b)->a[2]-b[2]);
+            int[] parent=new int[v];
+			for(int i=0;i<v;i++) parent[i]=i;
+			int total=0;
+			for(int i=0;i<e;i++){
+				if(union(edges[i][0],edges[i][1],parent)){
+					total+=edges[i][2];
+				}
+			}
+			System.out.println(total);
+			t--;
+		}
+
 	}
+
 }
